745dbd000b72b365efdeb6b0af0c2ae9903837eb
Andrew Arpasi <andrew@andrewarpasi.com>
2017-12-29 21:47:11 -0500

    Log total buys and sells over time

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const bittrex = require('node-bittrex-api')
 const secrets = require('./secrets')
 
+bittrex.options({
+    'apikey' : secrets.key,
+    'apisecret' : secrets.secret,
+})
+
+let totalBuys = 0
+let totalSells = 0
+
 bittrex.websockets.client(() => {
     console.log("Socket connected")
 
     bittrex.websockets.subscribe(['BTC-XRP'], (data) => {
-        console.log(data)
+        if(data.M === 'updateExchangeState') {
+            //console.log(data.A[0].Buys.length + " new buys",data.A[0].Sells.length + " new sells")
+            totalBuys += data.A[0].Buys.length
+            totalSells += data.A[0].Sells.length
+            console.log(totalBuys + " Total buys " + totalSells + " Total sells ")
+        }
     })
 })
