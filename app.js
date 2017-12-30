a3a89ea7b23533b35327cb69b77429e1886f33ee
Andrew Arpasi <andrew@andrewarpasi.com>
2017-12-29 21:32:20 -0500

    set up websockets

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const bittrex = require('node-bittrex-api')
 const secrets = require('./secrets')
 
+bittrex.websockets.client(() => {
+    console.log("Socket connected")
 
-
+    bittrex.websockets.subscribe(['BTC-XRP'], (data) => {
+        console.log(data)
+    })
+})
