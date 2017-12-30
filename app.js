1ca88a75daf3540cdf394414b978d03b1ee820aa
Andrew Arpasi <andrew@andrewarpasi.com>
2017-12-29 22:54:22 -0500

    average buy and sell rates

--- app.js
+++ app.js
@@ -9,18 +9,41 @@ bittrex.options({
 let totalBuys = 0
 let totalSells = 0
 let buySellRatio = 0
+let sellBuyRatio = 0
+let averageBuyRate = 0
+let averageSellRate = 0
 
+let BTC = 1
+let XRP = 0
+
+console.log("Awaiting connection to Bittrex...")
 bittrex.websockets.client(() => {
     console.log(new Date().toString())
     console.log("Socket connected")
 
     bittrex.websockets.subscribe(['BTC-XRP'], (data) => {
         if(data.M === 'updateExchangeState') {
+            //console.log(data.A[0].Buys)
+            const buys = data.A[0].Buys
+            const sells = data.A[0].Sells
             //console.log(data.A[0].Buys.length + " new buys",data.A[0].Sells.length + " new sells")
             totalBuys += data.A[0].Buys.length
             totalSells += data.A[0].Sells.length
             buySellRatio = totalBuys / totalSells
+            sellBuyRatio = totalSells / totalBuys
+            averageBuyRate = addRateToAverage(averageBuyRate,totalBuys,buys)
+            averageSellRate = addRateToAverage(averageSellRate,totalSells,sells)
             console.log(totalBuys + " Total buys " + totalSells + " Total sells " + buySellRatio + " Buy sell ratio ")
+            console.log(averageBuyRate + " Avg. Buy (BTC) " + averageSellRate + " Avg. Sell (BTC)")
         }
     })
 })
+
+const addRateToAverage = (oldAverage,oldSize,arr) => {
+    let average = oldAverage
+    let size = oldSize
+    arr.map((item) => {
+        average = average + ((item.Rate - average) / ++size)
+    })
+    return average
+}
